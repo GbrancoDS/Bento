1c70a3319536e929f0c9bafaf052dec4d36fc6c6
GbrancoDS <gbrancods@gmail.com>
2021-02-24 10:54:15 -0300

    Update weather.js
    Add latitude and longitude for Brazil (Votuporanga-SP)

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = '2132ff63894dac4714a9baf24ebba8e3';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = -20.440416335650035;
+  let longitude = -49.9156789238164;
 
   getWeather(latitude, longitude);
 }
